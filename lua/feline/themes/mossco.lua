e0d81a557ac894075cf233c0417b8b1c7ec9be92
tar80 <tar80master@gmail.com>
2023-08-12 14:17:57 +0900

    feat: adjust feline theme

--- lua/feline/themes/mossco.lua
+++ lua/feline/themes/mossco.lua
@@ -1,16 +1,28 @@
 local M = {}
 M.theme = {
-    fg = "#4B7774",
+    fg = "#678D8A",
     bg = "#173334",
     bg2 = "#215053",
-    cyan = "#56909A",
-    blue = "#437995",
-    green = "#5B8C6B",
-    olive = "#98905E",
-    orange = "#906B64",
-    pink = "#AA616D",
-    purple = "#796E99"
+    cyan = "#67B0B5",
+    blue = "#628EBB",
+    green = "#79A269",
+    olive = "#A89E67",
+    orange = "#BB8970",
+    pink = "#C36A74",
+    purple = "#A176B5"
 }
+-- M.theme = {
+--     fg = "#4B7774",
+--     bg = "#173334",
+--     bg2 = "#215053",
+--     cyan = "#70A7B0",
+--     blue = "#437995",
+--     green = "#5B8C6B",
+--     olive = "#98905E",
+--     orange = "#906B64",
+--     pink = "#AA616D",
+--     purple = "#796E99"
+-- }
 M.vi_mode = {
     NORMAL = M.theme.fg,
     INSERT = M.theme.cyan,
